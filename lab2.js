f16dc4bc87e8790652286bf0d9bc5e039542930a
Slevin Zhang <4ce111@gmail.com>
2016-01-19 12:27:39 -0500

    Complete exercise with bonus feature

--- lab2.js
+++ lab2.js
@@ -5,3 +5,21 @@ computerChoice = computerChoice < .33 ? 'rock' :
     computerChoice < .66 ? 'paper' : 'scissors';
 
 console.log(computerChoice);
+
+function compare(choice1, choice2) {
+  var choices = {'rock':1, 'paper':1, 'scissors':1};
+  var userChoice = choice1.trim().toLowerCase();
+  if (!(userChoice in choices))
+    return "You did not enter rock, paper, or scissors.";
+  if (choice1 == choice2)
+    return "The result is a tie!";
+  if (choice1 == 'rock')
+    return choice2 == 'scissors' ? 'rock wins' : 'paper wins';
+  if (choice1 == 'paper')
+    return choice2 == 'scissors' ? 'scissors wins' : 'paper wins';
+  if (choice1 == 'scissors')
+    return choice2 == 'rock' ? 'rock wins' : 'scissors wins';
+}
+
+var result = compare(userChoice, computerChoice);
+console.log(result);
